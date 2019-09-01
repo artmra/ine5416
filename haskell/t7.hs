ffdd7a5c684871210de14326dee48246f06d1e50
Arthur Moreira Rodrigues Alves <artmr.alves@gmail.com>
2019-09-01 12:18:54 -0300

    Mais alguns exercícios

--- haskell/t7.hs
+++ haskell/t7.hs
@@ -1,8 +1,15 @@
-fibonacci :: int -> int
-fibonacci n = if (n <= 1) then 1 else (fibonacci n - 1) + (fibonacci n - 2)
+fibonacci :: Int -> Int
+fibonacci n =
+    if n == 0 then
+        0
+    else
+        if n == 1 then
+            1
+        else
+            fibonacci(n - 1) + fibonacci(n - 2)
 
 main = do
-    print("Insira a posição do n-ésimo número da sequência de Fibonnaci que você deseja calcular: ")
+    print("Insira a posicao do n-esimo numero da sequencia de Fibonnaci que voce deseja calcular: ")
     n <- getLine
     let nInt = (read n :: Int)
     print (fibonacci nInt)
