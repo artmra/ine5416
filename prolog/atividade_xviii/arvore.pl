87a5640fed604924f67c7501ed8151d5ac92f7f1
Arthur Moreira Rodrigues Alves <artmr.alves@gmail.com>
2019-11-09 19:36:05 -0300

    indo.

--- prolog/atividade_xviii/arvore.pl
+++ prolog/atividade_xviii/arvore.pl
@@ -21,7 +21,8 @@ membro(N, no(_, ArvE, ArvD)) :-
     membro(N, ArvE), !;
     membro(N, ArvD).
     
-min(X,Y,R) :- (X =< Y, R is X); R is Y.
+min(X,Y,X) :- X =< Y, !.
+min(X,Y,Y).
 
 minimo(nil,1000). %INF = 1000
 minimo(no(N, ArvE, ArvD),Min) :-
@@ -35,6 +36,35 @@ caminho(Y, no(K, ArvE, ArvD), [K|Caminho]) :-
     caminho(Y, ArvE, Caminho), !;
     caminho(Y, ArvD, Caminho).
 
-distancia(no(Y, _, _), Y, 0) :- !.
-distancia(no(_, E, D), Y, Dist) :- (distancia(E, Y, Dist1); distancia(D, Y, Dist2)),
-                                   ((Dist1 =< Dist2, Dist is (Dist1 + 1)); Dist is (Dist2 + 1)).
+
+% ?- no(N, E, D), ocorrencias(55, no(N, E, D), Oco).
+% N = 52,
+% E = no(32, no(12, nil, nil), no(35, nil, nil)),
+% D = no(56, no(55, no(55, nil, nil), nil), no(64, nil, nil)),
+% Oco = 2 .
+ocorrencias(X, no(Y, nil, nil), N) :- ((X \== Y, N is 0); (X == Y, N is 1)), !.
+ocorrencias(X, no(Y, ArvE, ArvD), N) :- ocorrencias(X, ArvE, N_E), 
+                                        ocorrencias(X, ArvD, N_D),
+                                        ((X =:= Y, N is 1 + N_E + N_D); N is N_E + N_D).
+ocorrencias(X, no(Y, ArvE, nil), N) :- ocorrencias(X, ArvE, N_E),
+                                       ((X =:= Y, N is 1 + N_E); N is N_E).
+ocorrencias(X, no(Y, nill, ArvD), N) :- ocorrencias(X, ArvD, N_D),
+                                       ((X =:= Y, N is 1 + N_D); N is N_D).
+
+% ?- no(N, E, D), maior(35, no(N, E, D), Num).
+% N = 52,
+% E = no(32, no(12, nil, nil), no(35, nil, nil)),
+% D = no(56, no(55, no(55, nil, nil), nil), no(64, nil, nil)),
+% Num = 5 .
+maior(X, no(Y, nil, nil), N) :- ((X < Y, N is 1); (N is 0)), !.
+maior(X, no(Y, ArvE, ArvD), N) :- maior(X, ArvE, N_E), 
+                                  maior(X, ArvD, N_D),
+                                  ((X < Y, N is 1 + N_E + N_D); N is N_E + N_D).
+maior(X, no(Y, ArvE, nil), N) :- maior(X, ArvE, N_E),
+                                       ((X < Y, N is 1 + N_E); N is N_E).
+maior(X, no(Y, nill, ArvD), N) :- maior(X, ArvD, N_D),
+                                       ((X < Y, N is 1 + N_D); N is N_D).
+
+% ?- no(N, E, D), num_elementos(no(N, E, D), Num).
+num_elementos(nill, N) :- N is 0.
+num_elementos(no(X, ArvE, ArvD), N) :- num_elementos(ArvE, N_E), num_elementos(ArvD, N_D), N is 1 + N_E + N_D.
--- prolog/atividade_xviii/q1.pl
+++ prolog/atividade_xviii/q1.pl
@@ -7,6 +7,19 @@
 distancia(ponto(X1, Y1), ponto(X2, Y2), Dist) :- Dist is sqrt((X2 - X1)^2 + (Y2 - Y1)^2).
 
 % ?- colineares(ponto(2, 1), ponto(0, -3), ponto(-2, -7)).
+% true.
 
 colineares(ponto(X1, Y1), ponto(X2, Y2), ponto(X3, Y3)) :- ((X1*Y2) + (Y1*X3) + (X2*X3) - (Y1*X2) - (X1*Y3) - (Y2*X3)) =:= 0.
 
+abs(X, Y) :- X < 0, Y is (-X).
+abs(X, X) :- X >= 0.
+
+formaTriangulo(ponto(X1, Y1), ponto(X2, Y2), ponto(X3, Y3)) :- distancia(ponto(X1, Y1), ponto(X2, Y2), A),
+															   distancia(ponto(X1, Y1), ponto(X3, Y3), B),
+															   distancia(ponto(X2, Y2), ponto(X3, Y3), C),
+															   B_C is (B - C), A_C is (A - C), A_B is (A - B),
+															   abs(B_C, B_C_), abs(A_C, A_C_), abs(A_B, A_B_),
+															   BmC is (B + C), AmC is (A + C), AmB is (A + B),
+															   A > B_C_, A < BmC, 
+															   B > A_C_, B < AmC,
+															   C > A_B_, C < AmB. 
