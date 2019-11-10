a122e0f0fc8b32e84f90a223426846cc418ff6e0
Arthur Moreira Rodrigues Alves <artmr.alves@gmail.com>
2019-11-10 20:40:49 -0300

    projeto XViii

--- prolog/atividade_xviii/arvore.pl
+++ prolog/atividade_xviii/arvore.pl
@@ -9,6 +9,16 @@ no(52,
             no(55,nil,nil),
             nil),
         no(64,nil,nil))).
+
+no(52, 
+    no(32,
+        no(22,nil,nil),
+        no(33,nil,nil)), 
+    no(57,
+        no(15,
+            no(65,nil,nil),
+            nil),
+        no(64,nil,nil))).
         
 soma(nil, 0).
 soma(no(N, ArvE, ArvD),Soma) :-
@@ -65,6 +75,137 @@ maior(X, no(Y, ArvE, nil), N) :- maior(X, ArvE, N_E),
 maior(X, no(Y, nill, ArvD), N) :- maior(X, ArvD, N_D),
                                        ((X < Y, N is 1 + N_D); N is N_D).
 
-% ?- no(N, E, D), num_elementos(no(N, E, D), Num).
-num_elementos(nill, N) :- N is 0.
-num_elementos(no(X, ArvE, ArvD), N) :- num_elementos(ArvE, N_E), num_elementos(ArvD, N_D), N is 1 + N_E + N_D.
+% ?- no(N, E, D), leng(no(N, E, D), Num).
+% N = 52,
+% E = no(32, no(12, nil, nil), no(35, nil, nil)),
+% D = no(56, no(55, no(55, nil, nil), nil), no(64, nil, nil)),
+% Num = 8.
+
+leng(nil, 0).
+leng(no(N, ArvE, ArvD), Length) :-
+    leng(ArvE,Length_E),
+    leng(ArvD,Length_D),
+    Length is 1 + Length_E + Length_D.
+
+% no(N, ArvE, ArvD), media(no(N, ArvE, ArvD), Media).
+% N = 52,
+% ArvE = no(32, no(12, nil, nil), no(35, nil, nil)),
+% ArvD = no(56, no(55, no(55, nil, nil), nil), no(64, nil, nil)),
+% Media = 45.125.
+
+media(no(N, ArvE, ArvD), Media) :- soma(no(N, ArvE, ArvD), Soma),
+                                   leng(no(N, ArvE, ArvD), Length),
+                                   Media is Soma/Length, !.
+
+% no(N, ArvE, ArvD), nao_contem(10, no(N, ArvE, ArvD)).
+% true.
+
+% no(N, ArvE, ArvD), nao_contem(55, no(N, ArvE, ArvD)).
+% false.
+
+
+nao_contem(X, no(N, ArvE, ArvD)) :- not(membro(X, no(N, ArvE, ArvD))).
+
+concat([], L, L).
+concat([H|T], L2, [H|Tail]) :- concat(T,L2,Tail).
+
+% no(N, ArvE, ArvD), lista(no(N, ArvE, ArvD), Lista).
+% N = 52,
+% ArvE = no(32, no(12, nil, nil), no(35, nil, nil)),
+% ArvD = no(56, no(55, no(55, nil, nil), nil), no(64, nil, nil)),
+% Lista = [52, 32, 12, 35, 56, 55, 55, 64].
+
+
+lista(nil, []) :- !.
+lista(no(N, ArvE, ArvD), [N|Tail]) :- lista(ArvE, Lista_E),
+                                      lista(ArvD, Lista_D),
+                                      concat(Lista_E, Lista_D, Tail).
+
+% no(N, ArvE, ArvD), lista_pos_ordem(no(N, ArvE, ArvD), Lista).
+% N = 52,
+% ArvE = no(32, no(12, nil, nil), no(35, nil, nil)),
+% ArvD = no(56, no(55, no(55, nil, nil), nil), no(64, nil, nil)),
+% Lista = [12, 35, 32, 55, 55, 64, 56, 52].
+
+
+lista_pos_ordem(nil, []) :- !.
+lista_pos_ordem(no(N, ArvE, ArvD), Lista) :- lista_pos_ordem(ArvE, Lista_E),
+                                             lista_pos_ordem(ArvD, Lista_D),
+                                             concat(Lista_E, Lista_D, Lista_ED),
+                                             concat(Lista_ED, [N], Lista).
+
+% no(N, ArvE, ArvD), lista_pre_ordem(no(N, ArvE, ArvD), Lista).
+% N = 52,
+% ArvE = no(32, no(12, nil, nil), no(35, nil, nil)),
+% ArvD = no(56, no(55, no(55, nil, nil), nil), no(64, nil, nil)),
+% Lista = [52, 32, 12, 35, 56, 55, 55, 64].
+
+
+lista_pre_ordem(nil, []) :- !.
+lista_pre_ordem(no(N, ArvE, ArvD), Lista) :- lista_pre_ordem(ArvE, Lista_E),
+                                             lista_pre_ordem(ArvD, Lista_D),
+                                             concat([N], Lista_E, Lista_NE),
+                                             concat(Lista_NE, Lista_D, Lista).
+
+% no(N, ArvE, ArvD), lista_in_ordem(no(N, ArvE, ArvD), Lista).
+% N = 52,
+% ArvE = no(32, no(12, nil, nil), no(35, nil, nil)),
+% ArvD = no(56, no(55, no(55, nil, nil), nil), no(64, nil, nil)),
+% Lista = [12, 32, 35, 52, 55, 55, 56, 64].
+
+lista_in_ordem(nil, []) :- !.
+lista_in_ordem(no(N, ArvE, ArvD), Lista) :- lista_in_ordem(ArvE, Lista_E),
+                                            lista_in_ordem(ArvD, Lista_D),
+                                            concat(Lista_E, [N], Lista_EN),
+                                            concat(Lista_EN, Lista_D, Lista).
+
+% ?- no(N, ArvE, ArvD), folhas(no(N, ArvE, ArvD), NFolhas).
+% N = 52,
+% ArvE = no(32, no(12, nil, nil), no(35, nil, nil)),
+% ArvD = no(56, no(55, no(55, nil, nil), nil), no(64, nil, nil)),
+% NFolhas = 4.
+
+folhas(nil, 0) :- !.
+folhas(no(_, nil, nil), 1) :- !.
+folhas(no(N, ArvE, ArvD), NFolhas) :- folhas(ArvE, NFolhas_E),
+                                      folhas(ArvD, NFolhas_D),
+                                      NFolhas is NFolhas_E + NFolhas_D.
+
+% no(N, ArvE, ArvD), lista_folhas(no(N, ArvE, ArvD), Lista).
+% N = 52,
+% ArvE = no(32, no(12, nil, nil), no(35, nil, nil)),
+% ArvD = no(56, no(55, no(55, nil, nil), nil), no(64, nil, nil)),
+% Lista = [12, 35, 55, 64].
+
+lista_folhas(nil, []) :- !.
+lista_folhas(no(N, nil, nil), [N]) :- !.
+lista_folhas(no(N, ArvE, ArvD), Lista) :- lista_folhas(ArvE, Lista_E),
+                                          lista_folhas(ArvD, Lista_D),
+                                          concat(Lista_E, Lista_D, Lista).
+
+% no(N, ArvE, ArvD), iguais(no(N, ArvE, ArvD), no(N, ArvE, ArvD), Iguais).
+% N = 52,
+% ArvE = no(32, no(12, nil, nil), no(35, nil, nil)),
+% ArvD = no(56, no(55, no(55, nil, nil), nil), no(64, nil, nil)),
+% Iguais = true.
+
+iguais(nil, nil, true) :- !.
+iguais(no(N1, ArvE1, ArvD1), no(N2, ArvE2, ArvD2), Iguais) :- N1 =:= N2,
+                                                      iguais(ArvE1, ArvE2, Iguais),
+                                                      iguais(ArvD1, ArvD2, Iguais), !.
+
+% no(N, ArvE, ArvD), altura(no(N, ArvE, ArvD), Altura).
+% N = 52,
+% ArvE = no(32, no(12, nil, nil), no(35, nil, nil)),
+% ArvD = no(56, no(55, no(55, nil, nil), nil), no(64, nil, nil)),
+% Altura = 4 .
+
+altura(nil, 0) :- !.
+altura(no(N, ArvE, ArvD), Altura) :- altura(ArvE, Altura_E),
+                                     altura(ArvD, Altura_D),
+                                     ((Altura_E > Altura_D, Altura is 1 + Altura_E); Altura is 1 + Altura_D).
+
+% caminhoXparaY(52, 64, Caminho) .
+% Caminho = [52, 56, 64] .
+
+caminhoXparaY(X, Y, Caminho) :- no(X, ArvE, ArvD), caminho(Y, no(X, ArvE, ArvD), Caminho).
